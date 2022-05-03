17e3489cb275fabe2fc7bd885e3e2e01428aa8f4
Vladimir Borchevskiy <arsenaljkeeee10@gmail.com>
2022-05-03 17:33:24 +0300

    Initial commit

---
+++ l29.js
@@ -0,0 +1,31 @@
+//let yolochka4 = 4;
+//let yolochka6 = 6;
+
+//let yolochka_name = "Zelyonaya"
+
+
+//let result = yolochka4 + yolochka6;
+
+//let result_2 = yolochka6 + yolochka_name;
+
+//console.log('result + ${yolochka4 + yolochka6}')
+
+//console.log(result)
+//console.log(result_2)
+
+let t = true
+let f = false
+
+if (f) {
+    let apples = 3+5
+    console.log("if true = " + apples)
+}
+    else if (t) 
+    { let milk = 2
+        console.log("else if = " + milk)
+    }
+else { 
+
+    let meat = 2+2
+    console.log("If not true = " + meat)
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+let t = true
+let f = false
+
+console.log(t + f)
+
+if (t) {
+    let apples = 3+5
+    console.log("If true = " apples)
+} else { 
+
+    let meat = 2+2
+    console.log("If not true = " + meat)
