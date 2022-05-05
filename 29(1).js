5513b8fbe2d72a18c9f07707abfc6705d7efce13
Vladimir Borchevskiy <63147325+arsenaLJkeeee@users.noreply.github.com>
2022-05-05 16:13:56 +0300

    Add files via upload
    Cycles, variables
    
    while, for, if, else

---
+++ 29(1).js
@@ -0,0 +1,42 @@
+//console.log("1 -- "+ true || true)
+//console.log(true || false)
+//console.log(false || true)
+
+
+
+//console.log(true && true);
+//console.log(true && false)
+
+let item_1 = 0;
+let item_2 = 20;
+//let item_2 = 20;
+//let item_3 = (item_2 >= 20)? console.log("Idi Kushat") : console.log("Hodi golodnyi")
+
+
+//if (item_1 > 3 || item_2 < 15 || item_3 !== 50) 
+
+//{console.log ("Idi Kushat")}
+//else 
+//{console.log ("Hodi golodnyi")}
+
+
+
+
+while(item_1 < 20 && item_2 < 30) 
+
+
+{
+    console.log(item_1, " = Idi Kushat")
+    console.log(item_2, " = -------")
+    item_1++
+    item_2++
+
+    
+}
+
+for (let i = 0; i < 10; i++){
+
+
+console.log(i , "= FOR")
+
+}
