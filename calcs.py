bb923ad77f432bfa7bf57c856fa37c782b2176ce
arsenaLJkeeee <arsenaljkeeee10@gmail.com>
2022-05-14 19:34:42 +0300

    returning vowels

--- calcs.py
+++ calcs.py
@@ -1,1 +1,7 @@
-start
+def get_vowels(String):
+    return [each for each in String if each in "aeiou"]
+get_vowels("animal") # [a, i, a]
+get_vowels("sky") # []
+get_vowels("football") # [o, o, a]
+
+//example of returning vowels
